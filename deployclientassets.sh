641bf3b36b952a9cb7ed59cafc36843a8977d920
Andy Sherwood <xinmyname@gmail.com>
2012-07-07 22:53:56 -0700

    Now using coffeescript instead of bash to deploy client assests

--- deployclientassets.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Deploying client assets."
-echo "backbone"
-cp ./node_modules/backbone/backbone.js ./assets/js/
-echo "underscore"
-cp ./node_modules/backbone/node_modules/underscore/underscore.js ./assets/js/
-
